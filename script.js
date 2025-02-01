349bfc30454a3d151c54f24f352f26dec969084e
Neel <neelmendapara@gmail.com>
2025-02-02 02:06:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
             const sectionMap = {
                 "Features": "featured-3",
                 "FAQs": "icon-grid",
-                "Reviews": "reviews"
+                "Reviews": "reviews",
+                "About" : "about_"
+
             };
 
             const targetSection = sectionMap[this.textContent.trim()];
@@ -32,17 +34,5 @@ AOS.init({
     easing: 'ease-out-cubic',
     once: true,
     offset: 100 // Start animation when 100px before element enters viewport
-});
-
-document.querySelectorAll(".nav-link").forEach(link => {
-    link.addEventListener("click", function (event) {
-        event.preventDefault();
-        const sectionId = this.getAttribute("href").substring(1);
-        const target = document.getElementById(sectionId);
-        window.scrollTo({
-            top: target.offsetTop - 70,
-            behavior: "smooth"
-        });
-    });
 });
 
