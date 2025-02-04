e503f13b84b9c46e93d937cc6a3e0e1694e50fcc
YUG SHAH <yugshah7777@gmail.com>
2025-02-04 13:54:36 +0530

    Update loginStudent.php

--- PHP/loginStudent.php
+++ PHP/loginStudent.php
@@ -55,7 +55,7 @@
                 </label>
                 </div>
                 <a href="./studenthome.php"><button class="btn w-100 py-2" type="button" style="background-color: #7B89B1; color: white;">Sign in</button></a>
-                <a href="./signupStudent.php"><button class="btn w-100 py-2 mt-2" type="button" style="background-color: #4A447C; color: white;">Create New Account</button></a>
+                <p style="margin-top: 0.5rem;">Do not have an account?   <a href="./signupStudent.php" style=" text-decoration: none;">Sign Up...</a></p>
             </form>
         </div>  
 
