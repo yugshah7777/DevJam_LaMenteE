d8e425a65737a84020102a1b078966e40ab3d184
YUG SHAH <yugshah7777@gmail.com>
2025-02-07 22:19:55 +0530

    Add files via upload

---
+++ PHP/studentProfile.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <link 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"
+        />
+        <link
+            rel="stylesheet"
+            href="style.css"
+        />
+    </head>
+
+    <body style="background-color: rgb(249, 247, 247);">
+        <!-- NavBar -->
+        <div class="container-fluid home">
+            <header class="d-flex flex-wrap justify-content-center py-3 border-bottom">
+                <div class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <img src="./logo2-removebg.png" alt="MenteE Logo" height="40vh"/>
+                <img src="./logo2-name-removebg.png" alt="MenteE" height="20vh" style="margin-left: 1.5vh;"/>
+                </div>
+        
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a href="./studentdashboard.php" class="nav-link" aria-current="page">Dashboard</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">LogOut</a></li>
+                </ul>
+            </header>
+        </div>
+
+
+        <!-- JavaScript -->
+        <script 
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
+    </body>
+</html>
