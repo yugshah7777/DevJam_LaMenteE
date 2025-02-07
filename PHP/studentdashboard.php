3a4290ba14fa07dc9da33a6016dec4bb1efc249d
YUG SHAH <yugshah7777@gmail.com>
2025-02-08 00:58:23 +0530

    Update studentdashboard.php

--- PHP/studentdashboard.php
+++ PHP/studentdashboard.php
@@ -89,6 +89,46 @@ if (!isset($_SESSION['loggedin_student']) || $_SESSION['loggedin_student'] != tr
             </div>
         </nav>
 
+        <!-- Profile -->
+        <div class="container rounded bg-white mt-5 mb-5">
+            <div class="row">
+                <div class="col-md-3 border-right">
+                    <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">Name</span><span class="text-black-50">email@gmail.com</span><span> </span></div>
+                </div>
+                <div class="col-md-5 border-right">
+                    <div class="p-3 py-5">
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            <u><h4 class="text-right">Profile</h4></u>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col-md-6"><label class="labels">Name</label></div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-12"><label class="labels">Mobile Number</label></div>
+                            <div class="col-md-12 mt-3"><label class="labels">Address Line 1</label></div>
+                            <div class="col-md-12 mt-3"><label class="labels">PinCode</label></div>
+                            <div class="col-md-12 mt-3"><label class="labels">State</label></div>
+                            <div class="col-md-12 mt-3"><label class="labels">Area</label></div>
+                            <div class="col-md-12 mt-3"><label class="labels">Address Line 2</label></div>
+                            <div class="col-md-12 mt-3"><label class="labels">Email ID</label></div>
+                            <div class="col-md-12 mt-3"><label class="labels">Education</label></div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-6"><label class="labels">State/Region</label></div>
+                            <div class="col-md-6"><label class="labels">Country</label></div>
+                        </div>
+                        <div class="mt-5 text-center"><a href="./studentProfileEdit.html"><button class="btn btn-primary profile-button" type="button">Edit Profile</button></a></div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="p-3 py-5">
+                        <div class="d-flex justify-content-between align-items-center experience"><span><u><h4>Experience</h4></u></div><br>
+                        <div class="col-md-12"><label class="labels">Experience</label></div> <br>
+                        <div class="col-md-12"><label class="labels">Additional Details</label></div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <!-- JavaScript -->
         <script 
