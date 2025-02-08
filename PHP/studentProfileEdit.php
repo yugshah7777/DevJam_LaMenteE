23a3866173c82599e65c30628561e239f68de103
Bhargav Miyani <bhargavmiyani2909@gmail.com>
2025-02-08 09:55:28 +0530

    Update studentProfileEdit.php

--- PHP/studentProfileEdit.php
+++ PHP/studentProfileEdit.php
@@ -1,23 +1,23 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile Form</title>
-    <link 
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
-        rel="stylesheet" 
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
-        crossorigin="anonymous"
-    />
-</head>
 
-<body style="background-color: rgb(249, 247, 247);">
-    <div class="container rounded bg-white mt-5 mb-5">
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <link 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"
+        />
+        <link
+            rel="stylesheet"
+            href="style.css"
+        />
+    </head>
+
+    <body style="background-color: rgb(249, 247, 247);">
+        <!-- NavBar -->
+        <div class="container-fluid home">
         <form action="save_profile.php" method="POST">
             <div class="row">
                 <div class="col-md-3 border-right text-center">
@@ -91,13 +91,12 @@ session_start();
                 </div>
             </div>
         </form>
-    </div>
+        </div>
 
-    <script 
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
-        crossorigin="anonymous">
-    </script>
-</body>
-</html>
-
+        <!-- JavaScript -->
+        <script 
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
+    </body>
+</html> 
