f13d78b91da54b4db65933998c0561c5d71dcfab
Bhargav Miyani <bhargavmiyani2909@gmail.com>
2025-02-08 08:20:33 +0530

    Update studentProfileEdit.php

--- PHP/studentProfileEdit.php
+++ PHP/studentProfileEdit.php
@@ -25,8 +25,8 @@
                 </div>
         
                 <ul class="nav nav-pills">
-                    <li class="nav-item"><a href="./student.html" class="nav-link" aria-current="page">Dashboard</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link">LogOut</a></li>
+                    <li class="nav-item"><a href="./studentdashboard.php" class="nav-link" aria-current="page">Dashboard</a></li>
+                    <li class="nav-item"><a href="./logout.php" class="nav-link">Log Out</a></li>
                 </ul>
             </header>
         </div>
